35c3c11ee4eeee206b79145d0ca4cbbdf5f3193d
Yoann VANITOU <yvanitou@gmail.com>
2020-03-30 14:06:48 +0200

    Delete arm/v6

--- build.sh
+++ build.sh
@@ -2,4 +2,4 @@
 
 QBITTORRENT_VERSION=4.2.2
 
-exec docker buildx build --build-arg=QBITTORRENT_VERSION=$QBITTORRENT_VERSION --pull --platform linux/arm/v6,linux/arm/v7,linux/386,linux/arm64,linux/amd64 --tag maltyxx/qbittorrent:latest --tag maltyxx/qbittorrent:$QBITTORRENT_VERSION --push .
+exec docker buildx build --build-arg=QBITTORRENT_VERSION=$QBITTORRENT_VERSION --pull --platform linux/arm/v7,linux/386,linux/arm64,linux/amd64 --tag maltyxx/qbittorrent:latest --tag maltyxx/qbittorrent:$QBITTORRENT_VERSION --push .
