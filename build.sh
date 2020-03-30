bb4bef93b13c2e10d14205b25fe6196b47bbca28
Yoann VANITOU <yvanitou@gmail.com>
2020-03-30 13:38:38 +0200

    Add v4.2.2

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/sh -e
 
-QBITTORRENT_VERSION=4.2.1
+QBITTORRENT_VERSION=4.2.2
 
 exec docker buildx build --build-arg=QBITTORRENT_VERSION=$QBITTORRENT_VERSION --pull --platform linux/arm/v6,linux/arm/v7,linux/386,linux/arm64,linux/amd64 --tag maltyxx/qbittorrent:latest --tag maltyxx/qbittorrent:$QBITTORRENT_VERSION --push .
