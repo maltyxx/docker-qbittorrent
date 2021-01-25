2ea3ded1531983456de409f41e68e418b5190505
Yoann Vanitou <yvanitou@gmail.com>
2021-01-25 14:57:41 +0100

    Add version 4.3.3

--- buildx.sh
+++ buildx.sh
@@ -1,6 +1,6 @@
 #!/bin/sh -e
 
-QBITTORRENT_VERSION=4.3.2
+QBITTORRENT_VERSION=4.3.3
 
-exec docker buildx build --build-arg=NPROC=1 --build-arg=QBITTORRENT_VERSION=$QBITTORRENT_VERSION --pull --platform linux/arm/v7,linux/386,linux/arm64,linux/amd64 --tag maltyxx/qbittorrent:latest --tag maltyxx/qbittorrent:$QBITTORRENT_VERSION --push .
+exec docker buildx build --build-arg=NPROC=2 --build-arg=QBITTORRENT_VERSION=$QBITTORRENT_VERSION --pull --platform linux/arm64,linux/amd64 --tag maltyxx/qbittorrent:latest --tag maltyxx/qbittorrent:$QBITTORRENT_VERSION --push .
 exec docker scan maltyxx/qbittorrent:$QBITTORRENT_VERSION
