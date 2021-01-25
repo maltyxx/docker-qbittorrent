6fbdda9f1826b6d5f9ab4936a3fe8a377b6af6e1
Yoann Vanitou <yvanitou@gmail.com>
2021-01-25 13:04:46 +0100

    Add docker scan

--- buildx.sh
+++ buildx.sh
@@ -3,3 +3,4 @@
 QBITTORRENT_VERSION=4.3.2
 
 exec docker buildx build --build-arg=NPROC=1 --build-arg=QBITTORRENT_VERSION=$QBITTORRENT_VERSION --pull --platform linux/arm/v7,linux/386,linux/arm64,linux/amd64 --tag maltyxx/qbittorrent:latest --tag maltyxx/qbittorrent:$QBITTORRENT_VERSION --push .
+exec docker scan maltyxx/qbittorrent:$QBITTORRENT_VERSION
