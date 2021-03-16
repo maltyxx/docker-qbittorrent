775552ae40886d4d38f4ec380a86dece8de59dbe
Yoann Vanitou <yvanitou@gmail.com>
2021-03-16 15:44:48 +0100

    Update

--- buildx.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh -e
-
-QBITTORRENT_VERSION=4.3.3
-
-exec docker buildx build --build-arg=NPROC=1 --build-arg=QBITTORRENT_VERSION=$QBITTORRENT_VERSION --pull --platform linux/arm/v7,linux/arm64,linux/amd64 --tag maltyxx/qbittorrent:latest --tag maltyxx/qbittorrent:$QBITTORRENT_VERSION --push .
-exec docker scan maltyxx/qbittorrent:$QBITTORRENT_VERSION
